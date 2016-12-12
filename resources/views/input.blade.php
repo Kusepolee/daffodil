99b4f4ab91619438800cdb49404af34d4155b7cd
Kris <kris@krisdeMacBook-Pro.local>
2016-12-12 12:31:46 +0800

    init
    init

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -88,7 +88,19 @@
 
     function scan()
     {
-        alert('fuck');
+        wx.scanQRCode({
+            desc: 'scanQRCode desc',
+            needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+            scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+            success: function (res) {
+               alert('fuck');
+            }
+            error: function(res){
+                  if(res.errMsg.indexOf('function_not_exist') > 0){
+                       alert('版本过低请升级')
+                    }
+             }
+        });
     }
 
     function close()
