65c5fb8c8f423d994efdc05253072bd0825fbf8b
Kris <kris@krisdeMacBook-Pro.local>
2016-12-12 12:28:40 +0800

    init
    init

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -84,21 +84,11 @@
 <script type="text/javascript" >
     wx.config(<?php echo $w->getSignature(true,['scanQRCode']); ?>);
 
+
+
     function scan()
     {
-        wx.scanQRCode({
-            desc: 'scanQRCode desc',
-            needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-            scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
-            success: function (res) {
-               alert(res);
-            }
-            error: function(res){
-                  if(res.errMsg.indexOf('function_not_exist') > 0){
-                       alert('版本过低请升级')
-                    }
-             }
-        });
+        alert('fuck');
     }
 
     function close()
