a4dfcb8d14efc0c6a5984f89a33e6da0d99ad56f
Kris <kris@krisdeMacBook-Pro.local>
2016-12-12 13:21:56 +0800

    init
    init

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -90,7 +90,7 @@
         needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
         scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
         success: function (res) {
-           $("#ca").val(res);
+           $("#ca").val(res.resultStr);
         }
 
     });
