47916cc93457439dc893a3d602913fb6b8514706
Kris <kris@krisdeMacBook-Pro.local>
2016-12-12 13:38:55 +0800

    init
    Init

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -82,17 +82,17 @@
 	</div>
 <script src={{ $url }} type="text/javascript" ></script>
 <script type="text/javascript" >
-    wx.config(<?php echo $w->getSignature(true,['scanQRCode']); ?>);
+    wx.config(<?php echo $w->getSignature(false,['scanQRCode']); ?>);
 
 
-    wx.scanQRCode({
-        needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+   wx.scanQRCode({
         desc: 'scanQRCode desc',
+        needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
         scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
         success: function (res) {
-           $("#ca").val(res['resultStr']);
+            var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+            $("#ca").val(result);
         }
-
     });
 
 
